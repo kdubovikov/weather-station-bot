956b4ab203d5089ae75a195d171ab00eae3ac768
Kirill Dubovikov <kdubovikov@cinimex.ru>
2020-04-12 14:17:28 +0300

    working version of client code that integrates telegram and MQTT APIs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+#![feature(async_closure)]
 mod settings;
 
-use rumqtt::{MqttClient, MqttOptions, QoS};
+use rumqtt::{MqttClient, MqttOptions, QoS, SecurityOptions};
 use std::{sync::Arc};
 use serde::{Serialize, Deserialize};
 use std::fmt::Display;
 use clap::{App, Arg};
 use crate::settings::Settings;
-
+use tbot::prelude::*;
+use tbot::contexts::fields::Message;
+use tokio::sync::mpsc;
+use std::collections::HashMap;
+use tbot::{errors::MethodCall, types::{chat, parameters::Text, parameters::ChatId, parameters::ImplicitChatId}};
+use std::ops::Deref;
+use tbot::types::Chat;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct WeatherMessage {
@@ -54,7 +61,8 @@ impl Display for WeatherMessage {
 
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let matches = App::new("Weather station bot")
                     .version("0.1.0")
                     .author("Kirill Dubovikov <dubovikov.kirill@gmail.com>")
@@ -70,25 +78,45 @@ fn main() {
     let config = matches.value_of("config").unwrap_or("config");
     let settings = Settings::new(config).expect("Error while reading settings"); 
 
-    println!("Conntcting to MQTT server at {}:{}/{}", settings.mqtt.host, settings.mqtt.port, settings.mqtt.topic_name);
-    let mqtt_options = MqttOptions::new("weather_station_bot", settings.mqtt.host, settings.mqtt.port);
-    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
-      
-    mqtt_client.subscribe(settings.mqtt.topic_name, QoS::AtLeastOnce).unwrap();
+    let mut bot = tbot::Bot::new(settings.telegram.token.clone());
 
-    println!("Waiting for notifications");
-    for notification in notifications {
-        
-        match notification {
-            rumqtt::Notification::Publish(publish) => {
-                let payload = Arc::try_unwrap(publish.payload).unwrap();
-                let text: String = String::from_utf8(payload).expect("Can't decode payload for notification"); 
-                println!("Recieved message: {}", text);
-                let msg: WeatherMessage = serde_json::from_str(&text).expect("Error while deserializing message from ESP");
-                println!("Deserialized message: {:?}", msg);
-                println!("{}", msg);
-            }
-            _ => println!("{:?}", notification)
-        }
+    // let (mut tx, mut rx) = std::sync::mpsc::channel();
+    let (mut tok_tx, mut tok_rx) = tokio::sync::mpsc::unbounded_channel();
+
+    tokio::spawn(async move {
+            println!("Conntcting to MQTT server at {}:{}/{}", settings.mqtt.host, settings.mqtt.port, settings.mqtt.topic_name);
+            let mut mqtt_options = MqttOptions::new("weather_station_bot", settings.mqtt.host, settings.mqtt.port);
+            let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+
+            mqtt_client.subscribe(settings.mqtt.topic_name, QoS::AtLeastOnce).unwrap();
+
+            tokio::task::spawn_blocking(move || {
+                println!("Waiting for notifications");
+                for notification in notifications {
+                    match notification {
+                        rumqtt::Notification::Publish(publish) => {
+                            let payload = Arc::try_unwrap(publish.payload).unwrap();
+                            let text: String = String::from_utf8(payload).expect("Can't decode payload for notification");
+                            println!("Recieved message: {}", text);
+                            let msg: WeatherMessage = serde_json::from_str(&text).expect("Error while deserializing message from ESP");
+                            println!("Deserialized message: {:?}", msg);
+                            println!("{}", msg);
+                            // tx.send(msg);
+                            tok_tx.send(msg);
+                        }
+                        _ => println!("{:?}", notification)
+                    }
+                }
+            });
+    });
+
+    println!("Waiting for messages");
+    while let Some(msg) = tok_rx.recv().await {
+        println!("Recieved new message — {:?}", msg);
+        let message_str = &format!("{}", msg);
+        let message = Text::plain(message_str);
+        bot.send_message(ChatId::from(114238258), message).call().await.expect("Error while sending message to the bot");
     }
+
+    bot.event_loop().polling().start().await.unwrap();
 }
