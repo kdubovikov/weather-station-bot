f39d5e10a60c0606cd9f101f9265483e49cbe6e3
Kirill Dubovikov <kdubovikov@cinimex.ru>
2020-05-13 06:00:43 +0300

    refactor: introduced more structure to the code

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,11 @@ mod settings;
 
 use crate::settings::Settings;
 use clap::{App, Arg};
-use rumqtt::{MqttClient, MqttOptions, QoS, SecurityOptions};
+use rumqtt::{MqttClient, MqttOptions, QoS, SecurityOptions, Notification, Receiver};
 use std::fs::File;
 use std::io::prelude::*;
+
+use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use std::sync::Arc;
 
 use tbot::{
@@ -26,6 +28,66 @@ fn read_file_to_bytes(path: &str) -> Vec<u8> {
     let mut buf = Vec::new();
     f.read_to_end(&mut buf).unwrap();
     buf
+}
+
+async fn connect_to_mqtt_server(settings: &Settings) -> Receiver<Notification> {
+    println!(
+        "Conntcting to MQTT server at {}:{}/{}",
+        settings.mqtt.host, settings.mqtt.port, settings.mqtt.topic_name
+    );
+
+    let ca_cert = read_file_to_bytes(&settings.tls.ca_cert);
+
+    let mqtt_options = MqttOptions::new(
+        "weather_station_bot",
+        &settings.mqtt.host,
+        settings.mqtt.port
+    )
+    .set_ca(ca_cert)
+    // .set_client_auth(client_cert, client_key)
+    .set_security_opts(SecurityOptions::UsernamePassword(
+        settings.mqtt.username.clone(),
+        settings.mqtt.password.clone(),
+    ));
+
+    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+
+    mqtt_client
+        .subscribe(&settings.mqtt.topic_name, QoS::AtLeastOnce)
+        .unwrap();
+
+    notifications
+
+}
+
+fn process_messages_from_device(notifications: &Receiver<Notification>, tok_tx: &UnboundedSender<EspWeatherMessage>) {
+    println!("Waiting for notifications");
+    for notification in notifications {
+        match notification {
+            rumqtt::Notification::Publish(publish) => {
+                let payload = Arc::try_unwrap(publish.payload).unwrap();
+                let text: String = String::from_utf8(payload)
+                    .expect("Can't decode payload for notification");
+                println!("Recieved message: {}", text);
+                let msg: EspWeatherMessage = serde_json::from_str(&text)
+                    .expect("Error while deserializing message from ESP");
+                println!("Deserialized message: {:?}", msg);
+                println!("{}", msg);
+                tok_tx.send(msg).unwrap();
+            }
+            _ => println!("{:?}", notification),
+        }
+    }
+}
+
+async fn send_message_to_telegram(msg: &EspWeatherMessage, bot: &Arc<tbot::Bot>) {
+    let message_str = &format!("{}", msg);
+    let message = Text::plain(message_str);
+    println!("Sending message to Telegram");
+    bot.send_message(ChatId::from(114238258), message)
+        .call()
+        .await
+        .expect("Error while sending message to the bot");
 }
 
 #[tokio::main]
@@ -49,84 +111,43 @@ async fn main() {
     let config = matches.value_of("config").unwrap_or("config");
     let settings = Settings::new(config).expect("Error while reading settings");
 
-    let bot = tbot::Bot::new(settings.telegram.token.clone());
+    let bot = Arc::new(tbot::Bot::new(settings.telegram.token.clone()));
 
     let (tok_tx, mut tok_rx) = tokio::sync::mpsc::unbounded_channel::<EspWeatherMessage>();
 
-    let db_path = settings.db_path.clone();
+    let db_path = Arc::new(settings.db_path.clone());
 
     tokio::spawn(async move {
-        println!(
-            "Conntcting to MQTT server at {}:{}/{}",
-            settings.mqtt.host, settings.mqtt.port, settings.mqtt.topic_name
-        );
-
-        let ca_cert = read_file_to_bytes(&settings.tls.ca_cert);
-
-        let mqtt_options = MqttOptions::new(
-            "weather_station_bot",
-            settings.mqtt.host,
-            settings.mqtt.port,
-        )
-        .set_ca(ca_cert)
-        // .set_client_auth(client_cert, client_key)
-        .set_security_opts(SecurityOptions::UsernamePassword(
-            settings.mqtt.username,
-            settings.mqtt.password,
-        ));
-
-        let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
-
-        mqtt_client
-            .subscribe(settings.mqtt.topic_name, QoS::AtLeastOnce)
-            .unwrap();
+        let notifications = connect_to_mqtt_server(&settings).await;
 
         tokio::task::spawn_blocking(move || {
-            println!("Waiting for notifications");
-            for notification in notifications {
-                match notification {
-                    rumqtt::Notification::Publish(publish) => {
-                        let payload = Arc::try_unwrap(publish.payload).unwrap();
-                        let text: String = String::from_utf8(payload)
-                            .expect("Can't decode payload for notification");
-                        println!("Recieved message: {}", text);
-                        let msg: EspWeatherMessage = serde_json::from_str(&text)
-                            .expect("Error while deserializing message from ESP");
-                        println!("Deserialized message: {:?}", msg);
-                        println!("{}", msg);
-                        tok_tx.send(msg).unwrap();
-                    }
-                    _ => println!("{:?}", notification),
-                }
-            }
+            process_messages_from_device(&notifications, &tok_tx);
         });
     });
 
+
     println!("Waiting for messages");
-    while let Some(msg) = tok_rx.recv().await {
-        println!("Recieved new message — {:?}", msg);
+    let bot_sender = bot.clone();
+    tokio::spawn(async move {
+        while let Some(msg) = tok_rx.recv().await {
+            println!("Recieved new message — {:?}", msg);
 
-        let message_str = &format!("{}", msg);
-        let message = Text::plain(message_str);
-        println!("Sending message to Telegram");
-        bot.send_message(ChatId::from(114238258), message)
-            .call()
-            .await
-            .expect("Error while sending message to the bot");
+            send_message_to_telegram(&msg, &bot_sender).await;
 
-        let c = db_path.clone();
+            let db = Arc::clone(&db_path);
 
-        tokio::task::spawn_blocking(move || {
-            println!("Saving message to DB");
-            let connection = establish_connection(&c); 
-            let new_log = msg.to_new_weather_message();
-            new_log.save_to_db(&connection).unwrap();
-            print!("Successfully saved message to DB");
-        });
-    }
+            tokio::task::spawn_blocking(move || {
+                println!("Saving message to DB");
+                let connection = establish_connection(&db); 
+                let new_log = msg.to_new_weather_message();
+                new_log.save_to_db(&connection).unwrap();
+                print!("Successfully saved message to DB");
+            });
+        }
+    });
 
-    let mut event_loop = bot.event_loop();
 
+    let mut event_loop = (*bot).clone().event_loop();
     event_loop.command("subscribe", |context| async move {
         let chat_id = context.chat.id;
         context
